a9ca44876a9a1955a4027b59028ea2c7c4278a16
oort <yaooort@gmail.com>
2023-11-29 00:45:39 +0800

    commit dd

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -1,13 +1,13 @@
-import { viteBundler } from "@vuepress/bundler-vite";
-import { defineUserConfig } from "vuepress";
-import { gungnirTheme } from "vuepress-theme-gungnir";
+import {viteBundler} from "@vuepress/bundler-vite";
+import {defineUserConfig} from "vuepress";
+import {gungnirTheme} from "vuepress-theme-gungnir";
 
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineUserConfig({
+  lang: 'zh-CN',
   title: "Oort",
-  description: "Oort is a dragon lost in human world.",
-
+  description: "笔墨间，岁月静好，吾心所向，字字珠玑，篇篇不朽。",
   head: [
     [
       "link",
@@ -27,42 +27,47 @@ export default defineUserConfig({
         href: `/img/logo/favicon-32x32.png`
       }
     ],
-    ["meta", { name: "application-name", content: "Xiaohan Zou" }],
-    ["meta", { name: "apple-mobile-web-app-title", content: "Xiaohan Zou" }],
+    ["meta", {name: "application-name", content: "Xiaohan Zou"}],
+    ["meta", {name: "apple-mobile-web-app-title", content: "Xiaohan Zou"}],
     [
       "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+      {name: "apple-mobile-web-app-status-bar-style", content: "black"}
     ],
     [
       "link",
-      { rel: "apple-touch-icon", href: `/img/logo/apple-touch-icon.png` }
+      {rel: "apple-touch-icon", href: `/img/logo/apple-touch-icon.png`}
     ],
-    ["meta", { name: "theme-color", content: "#377bb5" }],
-    ["meta", { name: "msapplication-TileColor", content: "#377bb5" }]
+    ["meta", {name: "theme-color", content: "#377bb5"}],
+    ["meta", {name: "msapplication-TileColor", content: "#377bb5"}]
   ],
 
   bundler: viteBundler(),
 
   theme: gungnirTheme({
-    repo: "Renovamen/oortk.com",
+    repo: "yaooort/yaooort",
     docsDir: "blog",
-    docsBranch: "master",
-
+    docsBranch: "main",
+    navbarTitle: "0.0",
     hitokoto: "https://v1.hitokoto.cn?c=i", // enable hitokoto (一言) or not?
-
+    searchText: "搜索",
     // personal information
     personalInfo: {
       name: "Oort",
       avatar: "/img/avatar.jpg",
-      description: "明日之光",
+      description: "我本将心照明月，奈何明月照沟渠",
       sns: {
-        github: "Renovamen",
-        linkedin: "xiaohan-zou",
-        // facebook: "renovamen.zou",
-        twitter: "renovamen_zxh",
-        zhihu: "chao-neng-gui-su",
-        email: "renovamenzxh@gmail.com",
-        rss: "/rss.xml"
+        github: "yaooort",
+        // linkedin: "yaooort",
+        // facebook: "yaooort",
+        // twitter: "yaooort",
+        // zhihu: "yaooort",
+        email: "yaooort@gmail.com",
+        rss: "/rss.xml",
+        // 添加其他的社交平台
+        bilibili: {  // 随便什么名字
+          icon: "ri-bilibili-line",  // 社交平台的图标
+          link: "https://www.bilibili.com/"  // 主页链接
+        }
       }
     },
 
@@ -122,8 +127,8 @@ export default defineUserConfig({
       mdPlus: {
         all: true
       },
-      ga: "G-HCQSX53XFG",
-      ba: "75381d210789d3eaf855fa16246860cc",
+      ga: "G-8MWRXR83EZ",
+      ba: "0063b84a6a9d21a9a3ae130a736d2193",
       rss: {
         siteURL: "https://oortk.com",
         copyright: "Renovamen 2018-2022"
@@ -132,27 +137,27 @@ export default defineUserConfig({
 
     navbar: [
       {
-        text: "Home",
+        text: "首页",
         link: "/",
         icon: "fa-fort-awesome"
       },
       {
-        text: "Tags",
+        text: "标签",
         link: "/tags/",
         icon: "fa-tag"
       },
       {
-        text: "Links",
+        text: "链接",
         link: "/links/",
         icon: "fa-satellite-dish"
       },
       {
-        text: "About",
+        text: "关于",
         link: "https://zxh.io",
         icon: "fa-paw"
       },
       {
-        text: "Portfolio",
+        text: "其他",
         link: "https://portfolio.zxh.io/",
         icon: "oi-rocket"
       }
@@ -161,9 +166,11 @@ export default defineUserConfig({
     footer: `
       &copy; <a href="https://github.com/Renovamen" target="_blank">Oort</a> 2009-2024
       <br>
-      Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
-      <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
-    `
+      Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> & <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
+      <br>
+      <a target="_blank" href="https://beian.miit.gov.cn">沪ICP备11018329号-5 / </a>
+      <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010402002252">沪公网安备31010402002252号  </a>
+     `
   }),
 
   markdown: {
